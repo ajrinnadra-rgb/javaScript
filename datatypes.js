c06f2fce25be790224eb2bf9a23dd94ee841de4d
Ajrin <ajrinnadra@gmail.com>
2025-12-13 21:27:35 +0600

    open first datatypes close

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+let ks = "hem";
+let mo = 1227;
+let de =  43767935985831n;
+console.log(de)
+let wi = false;
+let os = null;
+let ur;
+console.log(ur)
+let eu = Symbol("cvsnm");
+//console.log(eu+de+wi+ks+os);
+console.log(wi+os) // 0 + 0
+
+
+// inteager 0, 1, 2, 3, ......
+// float 2.45, 5.48,,,
+// type o
+/*typeof eu
+typeof 0.06
+typeof true
+Array.isarray[()] */
+console.log(typeof eu + typeof 0.06 + typeof true + Array.isArray([]) )
+console.log(Array.isArray([]) == 0) // out 
